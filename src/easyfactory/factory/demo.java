a0108c798504155dc6945ddc94c0285aa3893030
liuboren0617 <13810113324@163.com>
2018-06-23 21:46:38 +0800

    easy factory

---
+++ src/easyfactory/factory/demo.java
@@ -0,0 +1,13 @@
+package easyfactory.factory;
+
+/**
+ * @author liuboren
+ * @Title:
+ * @Description:
+ * @date 2018/6/23 21:45
+ */
+public class demo {
+    public static void main(String [] args){
+        System.out.println("this is test");
+    }
+}
