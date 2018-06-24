f15d116ad6987168f1c601d0975de0fdf81d688d
liuboren0617 <13810113324@163.com>
2018-06-24 21:37:17 +0800

    update

--- src/strategy/test/Test.java
+++ src/strategy/test/Test.java
@@ -1,28 +1,28 @@
 package strategy.test;
 
-import strategy.context.Context;
-import strategy.strategy.impl.ConcreteStrategyA;
-import strategy.strategy.impl.ConcreteStrategyB;
-import strategy.strategy.impl.ConcreteStrategyC;
+        import strategy.context.Context;
+        import strategy.strategy.impl.ConcreteStrategyA;
+        import strategy.strategy.impl.ConcreteStrategyB;
+        import strategy.strategy.impl.ConcreteStrategyC;
 
 /**
  * @author liuboren
- * @Title:
+ * @Title:测试类
  * @Description:
  * @date 2018/5/30 14:57
  */
 public class Test {
-        public static void main(String [] args){
-                    Context context;
+    public static void main(String [] args){
+        Context context;
 
-                    context = new Context(new ConcreteStrategyA());
-                    context.contextInterface();
+        context = new Context(new ConcreteStrategyA());
+        context.contextInterface();
 
-                    context = new Context(new ConcreteStrategyB());
-                    context.contextInterface();
+        context = new Context(new ConcreteStrategyB());
+        context.contextInterface();
 
-                    context = new Context(new ConcreteStrategyC());
-                    context.contextInterface();
+        context = new Context(new ConcreteStrategyC());
+        context.contextInterface();
 
-        }  
+    }
 }
